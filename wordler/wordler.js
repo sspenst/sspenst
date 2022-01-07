f6120dcc35ca2ee8cac36de987a2b5e52d207744
Spencer Spenst <sspenst@Spencers-MacBook-Pro.local>
2022-01-06 20:14:54 -0800

    wordler lol

---
+++ wordler/wordler.js
@@ -0,0 +1,32 @@
+var answer = 'APPLE';
+
+function makeGuess() {
+    var guess = document.getElementById('guess').value;
+
+    if (guess.length != 5) {
+        return;
+    }
+
+    guess = guess.toUpperCase();
+
+    var guessFrag = document.createDocumentFragment();
+    var newDiv = document.createElement('div');
+    var guessStr = '';
+
+    for (var i = 0; i < guess.length; i++) {
+        var letter = guess[i];
+
+        if (letter === answer[i]) {
+            guessStr = guessStr + '<span class="green">' + letter + '</span>';
+        } else if (answer.includes(letter)) {
+            guessStr = guessStr + '<span class="yellow">' + letter + '</span>';
+        } else {
+            guessStr = guessStr + letter;
+        }
+    }
+
+    newDiv.innerHTML = guessStr;
+    guessFrag.appendChild(newDiv);
+    document.getElementById('guesses').appendChild(guessFrag);
+    document.getElementById('guess').value = '';
+};
