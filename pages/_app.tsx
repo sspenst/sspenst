d0e857591b78f48ac96b3d516fe04864de4ad582
Spencer Spenst <spencerspenst@gmail.com>
2022-12-11 21:04:41 -0800

    fix mobile animation bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/global.css';
 import { Work_Sans } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 const workSans = Work_Sans({
   fallback: ['system-ui', 'arial'],
@@ -11,34 +11,22 @@ const workSans = Work_Sans({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  const ssPathLength = 168.26;
-  const [style, setStyle] = useState<React.CSSProperties>({
-    strokeDashoffset: ssPathLength,
-    transition: 'none',
-  });
-
-  useEffect(() => {
-    setStyle({
-      strokeDashoffset: 0,
-      transition: 'stroke-dashoffset 1.25s ease',
-    });
-  }, []);
-
   return (
     <main className={workSans.className}>
       <Head>
         <title>Spencer Spenst</title>
       </Head>
       <div className='flex fixed inset-0 items-center justify-center text-center font-semibold text-7xl animateSS'>
-        <div className='flex flex-row w-48'>
-          <svg viewBox="0 0 30 30" xmlns="http://www.w3.org/2000/svg">
+        <div className='flex flex-row w-48 h-48'>
+          <svg viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'>
             <path
-              fill="none"
-              stroke="white"
-              strokeWidth="0.15"
-              strokeDasharray={ssPathLength}
-              style={style}
-              d="M 17.2,28
+              fill='none'
+              stroke='white'
+              className='animateSSPath'
+              strokeWidth='0.15'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M 17.2,28
               q -0.4,1 -1.4,1
               h -12.8
               q -1,0 -0.6,-1
@@ -74,7 +62,7 @@ export default function App({ Component, pageProps }: AppProps) {
               q -0.4,1 0.6,1
               h 8.8
               q 1,0 0.6,1
-              Z"
+              Z'
             />
           </svg>
         </div>
