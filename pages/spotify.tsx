47613ed1071c7931682e6f5c5b723e4b7a3c80a6
Spencer Spenst <spencerspenst@gmail.com>
2023-06-14 16:36:39 -0700

    some cleanup

--- pages/spotify.tsx
+++ pages/spotify.tsx
@@ -6,7 +6,7 @@ import FormattedTrack from '../components/spotifyRabbit/formattedTrack';
 import FormattedUser from '../components/spotifyRabbit/formattedUser';
 import { SpotifyRabbitContext } from '../contexts/spotifyRabbitContext';
 import { loadTokens, redirectToAuthCodeFlow, removeTokens, spotifyFetch } from '../helpers/authCodeWithPkce';
-import { parseTrack, parseTracks, parseUser, Track, User } from '../helpers/spotifyParsers';
+import { parseTracks, parseUser, Track, User } from '../helpers/spotifyParsers';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
@@ -23,13 +23,12 @@ interface SpotifyProps {
 export default function Spotify({ code }: SpotifyProps) {
   const [disableGetTracks, setDisableGetTracks] = useState(false);
   const [disableSave, setDisableSave] = useState(false);
-  const [initialTrackId, setInitialTrackId] = useState('57RJ51keAi2GaYOPtaTjfT');
   const limit = 20;
   const myTracksOffset = useRef(0);
+  const [playlist, setPlaylist] = useState<Track[]>([]);
   const [preview, setPreview] = useState<HTMLAudioElement>();
   const [recommendations, setRecommendations] = useState<Track[]>([]);
   const router = useRouter();
-  const [tracks, setTracks] = useState<Track[]>([]);
   const [user, setUser] = useState<User>();
 
   async function loadMyTracks() {
@@ -79,22 +78,6 @@ export default function Spotify({ code }: SpotifyProps) {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  async function getTrack(id: string) {
-    const res = await spotifyFetch(`https://api.spotify.com/v1/tracks/${id}`, {
-      method: 'GET',
-    });
-
-    const track = parseTrack(await res.json());
-
-    setTracks(prev => {
-      const newTracks = [...prev];
-
-      newTracks.push(track);
-
-      return newTracks;
-    });
-  }
-
   function logOut() {
     removeTokens();
     router.push('/');
@@ -103,11 +86,11 @@ export default function Spotify({ code }: SpotifyProps) {
   async function getTracks() {
     setDisableGetTracks(true);
 
-    if (tracks.length === 0) {
+    if (playlist.length === 0) {
       await loadMyTracks();
     } else {
       // TODO: use as many latest tracks + genres as possible?
-      const latestTrack = tracks[tracks.length - 1];
+      const latestTrack = playlist[playlist.length - 1];
       const res = await spotifyFetch(`https://api.spotify.com/v1/recommendations?${new URLSearchParams({
         limit: String(limit),
         seed_artists: latestTrack.artists.map(a => a.id).slice(0, 5).join(','),
@@ -124,7 +107,7 @@ export default function Spotify({ code }: SpotifyProps) {
   }
 
   async function save() {
-    if (!user || !tracks.length) {
+    if (!user || !playlist.length) {
       return;
     }
 
@@ -133,7 +116,7 @@ export default function Spotify({ code }: SpotifyProps) {
     const createPlaylistRes = await spotifyFetch(`https://api.spotify.com/v1/users/${user.id}/playlists`, {
       body: JSON.stringify({
         description: 'Playlist created with Set Builder',
-        name: `Set Builder - ${tracks[0].name}`,
+        name: `Set Builder - ${playlist[0].name}`,
         public: false,
       }),
       headers: {
@@ -142,15 +125,15 @@ export default function Spotify({ code }: SpotifyProps) {
       method: 'POST',
     });
 
-    const playlist = await createPlaylistRes.json();
+    const spotifyPlaylist = await createPlaylistRes.json();
 
     // add all tracks to the playlist
     // TODO: loop through if above POST limit (100 per call)
 
-    await spotifyFetch(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
+    await spotifyFetch(`https://api.spotify.com/v1/playlists/${spotifyPlaylist.id}/tracks`, {
       body: JSON.stringify({
         position: 0,
-        uris: tracks.map(t => t.uri),
+        uris: playlist.map(t => t.uri),
       }),
       headers: {
         'Content-Type': 'application/json',
@@ -194,14 +177,14 @@ export default function Spotify({ code }: SpotifyProps) {
       setPreview: setPreview,
     }}>
       <div className='flex inset-0 fixed select-none'>
-        {tracks.length > 0 &&
+        {playlist.length > 0 &&
           <div className='flex flex-col my-3 ml-3 gap-3'>
             <div className='grow w-96 bg-neutral-800 rounded-md flex flex-col p-2 overflow-y-scroll items-center'>
-              {tracks.map((track, trackIndex) => (
+              {playlist.map((track, trackIndex) => (
                 <div className='flex items-center w-full hover:bg-neutral-700 transition py-1 pr-4 pl-2 gap-4 rounded-md' key={`track-${trackIndex}`}>
                   <FormattedTrack track={track} />
                   <div className={classNames('transition cursor-pointer hover:scale-125 hover:text-red-500')} onClick={() => {
-                    setTracks(prevTracks => {
+                    setPlaylist(prevTracks => {
                       const newTracks = [...prevTracks];
 
                       newTracks.splice(trackIndex, 1);
@@ -232,14 +215,22 @@ export default function Spotify({ code }: SpotifyProps) {
               disabled={disableGetTracks}
               onClick={() => getTracks()}
             >
+              {/* TODO:
+              if playlist is empty:
+              - left/right arrow for getting my tracks using offset
+              - future: search bar to find any song
+              otherwise:
+              - search icon
+              - add parameters for recommendations
+              */}
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
               </svg>
             </button>
-            <div className='flex gap-3'>
+            <div className='flex truncate'>
               <FormattedUser user={user} />
               <button
-                className='bg-neutral-500 text-black p-3 text-2xl rounded-full hover:bg-neutral-300 transition'
+                className='text-neutral-400 p-3 text-2xl rounded-full hover:bg-neutral-700 transition'
                 onClick={() => logOut()}
               >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -265,7 +256,7 @@ export default function Spotify({ code }: SpotifyProps) {
             {recommendations.map(track => (
               <div className='flex items-center w-full hover:bg-neutral-700 transition py-1 px-4 gap-4 rounded-md' key={`recommended-track-${track.id}`}>
                 <div className='transition cursor-pointer hover:scale-125 hover:text-green-500' onClick={() => {
-                  setTracks(prevTracks => {
+                  setPlaylist(prevTracks => {
                     const newTracks = [...prevTracks];
 
                     newTracks.push({ ...track });
