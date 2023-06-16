c801fced5d8232cd91af89a2db7384cf77a738c1
Spencer Spenst <spencerspenst@gmail.com>
2023-06-15 21:50:11 -0700

    unoptimized images

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
         pathname: '/image/*',
         protocol: 'https',
       },
-    ]
+    ],
+    // https://vercel.com/docs/concepts/image-optimization/managing-image-optimization-costs#how-to-minimize-image-optimization-costs
+    // to avoid hitting the image optimization limit on vercel's free tier
+    unoptimized: true,
   },
   swcMinify: true,
 };
