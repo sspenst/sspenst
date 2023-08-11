7e2981614d02a1f161f6d220d8fb9c7937d29f14
Spencer Spenst <spencerspenst@gmail.com>
2023-08-10 22:40:37 -0700

    favicon tweaks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Background from '../components/background';
 import Footer from '../components/footer';
 import Header from '../components/header';
@@ -16,6 +16,24 @@ const inter = Inter({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    function setFaviconHref(e: MediaQueryList | MediaQueryListEvent) {
+      const favicon = document.getElementById('favicon') as HTMLLinkElement | null;
+
+      if (favicon) {
+        favicon.href = e.matches ? '/favicon-white.png' : 'favicon.png';
+      }
+    }
+
+    const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    setFaviconHref(darkMediaQuery);
+
+    darkMediaQuery.addEventListener('change', setFaviconHref);
+
+    return () => darkMediaQuery.removeEventListener('change', setFaviconHref);
+  }, []);
+
   return (
     <div className={classNames(inter.className, 'overflow-hidden')}>
       <ThemeProvider attribute='class'>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,8 +13,8 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='manifest' href='/manifest.json' />
-          <link rel='apple-touch-icon' href='/ss-192-maskable.png' />
-          <link rel='shortcut icon' href='/ss-48.png' />
+          <link rel='apple-touch-icon' type='image/png' href='/ss-192-maskable.png' />
+          <link rel='icon' type='image/png' href='/favicon.png' id='favicon' />
           <meta name='theme-color' content='rgb(23 23 23)' />
         </Head>
         <body className='bg-white dark:bg-neutral-900 text-black dark:text-white'>
