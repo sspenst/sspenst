b7667541c08d0784bcc370450d21944c957c9564
Spencer Spenst <spencerspenst@gmail.com>
2023-08-15 20:40:06 -0700

    favicon.ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
         <Head>
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' type='image/png' href='/ss-192-maskable.png' />
-          <link rel='icon' type='image/png' href='/favicon.png' id='favicon' />
+          <link rel='icon' href='/favicon.ico' />
           <meta name='theme-color' content='rgb(23 23 23)' />
         </Head>
         <body className='bg-white dark:bg-neutral-900 text-black dark:text-white'>
