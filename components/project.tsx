cb0e7b9cfa764d951a6512d6fe5fcbd63d870c35
Spencer Spenst <spencerspenst@gmail.com>
2023-09-15 22:13:06 -0700

    project descriptions + ux updates

--- components/project.tsx
+++ components/project.tsx
@@ -2,63 +2,59 @@ import Image from 'next/image';
 import React from 'react';
 
 interface ProjectProps {
-  bgColor?: string;
-  description: string;
+  bgColor: string;
+  description: string | JSX.Element;
   href: string;
   id: string;
+  srcIcon: string;
+  srcPreview?: string;
   title: string;
 }
 
-export default function Project({ bgColor, description, href, id, title }: ProjectProps) {
-  const titleLower = title.toLowerCase();
-
+export default function Project({ bgColor, description, href, id, srcIcon, srcPreview, title }: ProjectProps) {
   return (
     <div className='flex flex-col items-start gap-8 w-fit max-w-full px-8 pt-20' id={id}>
-      <div className='col-span-2 flex gap-8 mx-4 w-full items-center'>
-        {bgColor &&
-          <a
-            className='border border-neutral-700 p-3 shadow-lg rounded-lg flex items-center justify-center hover:scale-105 transition'
-            href={href}
-            rel='noreferrer'
-            style={{
-              backgroundColor: bgColor,
-            }}
-            target='_blank'
-          >
-            <Image alt={title} src={`/${titleLower}.svg`} width='32' height='32' style={{
-              minHeight: 32,
-              minWidth: 32,
-            }} />
-          </a>
-        }
-        <div className='flex flex-col gap-2'>
-          <a
-            className='hover:underline text-3xl font-medium w-fit'
-            href={href}
-            rel='noreferrer'
-            target='_blank'
-          >
-            {title}
-          </a>
-          <span>{description}</span>
-        </div>
+      <div className='flex gap-4 mx-2 items-center'>
+        <a
+          className='border border-neutral-700 p-2.5 shadow-lg rounded-lg flex items-center justify-center hover:scale-105 transition'
+          href={href}
+          rel='noreferrer'
+          style={{
+            backgroundColor: bgColor,
+          }}
+          target='_blank'
+        >
+          <Image alt={title} src={srcIcon} width='28' height='28' style={{
+            minHeight: 28,
+            minWidth: 28,
+          }} />
+        </a>
+        <a
+          className='hover:underline text-3xl font-medium w-fit'
+          href={href}
+          rel='noreferrer'
+          target='_blank'
+        >
+          {title}
+        </a>
       </div>
-      <div className='col-span-3 flex justify-center max-w-full'>
-        {bgColor &&
-          <a
-            className='hover:opacity-70 transition p-4 max-w-full w-full rounded-xl border border-neutral-700 hover:scale-[1.01]'
-            href={href}
-            rel='noreferrer'
-            style={{
-              backgroundColor: bgColor,
-            }}
-            target='_blank'
-          >
-            <div className='flex items-center max-w-full aspect-video overflow-hidden'>
-              <Image alt={`${title} Preview`} src={`/${titleLower}-preview.png`} width={512} height={1} priority />
-            </div>
-          </a>
-        }
+      {srcPreview &&
+        <a
+          className='hover:opacity-70 transition p-4 max-w-full rounded-xl border border-neutral-700 hover:scale-[1.01]'
+          href={href}
+          rel='noreferrer'
+          style={{
+            backgroundColor: bgColor,
+          }}
+          target='_blank'
+        >
+          <div className='flex items-center max-w-full aspect-video overflow-hidden'>
+            <Image alt={`${title} Preview`} src={srcPreview} width={512} height={1} priority />
+          </div>
+        </a>
+      }
+      <div className='mx-2'>
+        {description}
       </div>
     </div>
   );
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -11,32 +11,51 @@ export default function Projects() {
     <div className='flex justify-center'>
       <div className='w-full max-w-7xl'>
         <div className='hidden lg:flex flex-col gap-2 fixed w-72 px-8 py-4 font-medium'>
-          <a className='hover:text-rose-500' href='#pathology'>Pathology</a>
-          <a className='hover:text-rose-500' href='#rabbit'>Rabbit</a>
-          <a className='hover:text-rose-500' href='#spotify'>Spotify Web API SDK</a>
+          <a className='hover:text-rose-500 transition w-fit' href='#pathology'>Pathology</a>
+          <a className='hover:text-rose-500 transition w-fit' href='#rabbit'>Rabbit</a>
+          <a className='hover:text-rose-500 transition w-fit' href='#spotify'>Spotify Web API SDK</a>
         </div>
         <div className='flex flex-col pb-8 lg:pl-72'>
           <h1 className='text-4xl font-medium px-8'>Projects</h1>
           <div className='flex'>
-            <div className='flex flex-col pb-8 items-center'>
+            <div className='flex flex-col pb-8'>
               <Project
                 bgColor='rgb(38 38 38)'
-                description='Shortest path puzzle game'
+                description={<>
+                  <span>The goal of Pathology is simple: Get to the exit in the least number of moves. Sounds easy right? Yet, this game is one of the most mind-bending puzzle games you will find. Different blocks stand in your way to the exit, and your job is to figure out the optimal route.</span>
+                  <br /><br />
+                  <span>An active community of over one thousand users has helped build over 5000 levels. A level and collection editor allows you to build your own challenging levels for the Pathology community. Pathology also has an official campaign, multiplayer, review system, leaderboards, and much more!</span>
+
+                </>}
                 href='https://pathology.gg'
                 id='pathology'
+                srcIcon='/pathology.svg'
+                srcPreview='/pathology-preview.png'
                 title='Pathology'
               />
               <Project
                 bgColor='black'
-                description='Discover new tracks using Spotify&apos;s audio features'
+                description='Discover new tracks using Spotify&apos;s audio features. Select a track to listen to a short preview, then click discover to find similar tracks. Refine your search with audio features and save tracks you enjoy! Rabbit has been approved by the Spotify Developer Platform.'
                 href='https://rabbit.sspenst.com'
                 id='rabbit'
+                srcIcon='/rabbit.svg'
+                srcPreview='/rabbit-preview.png'
                 title='Rabbit'
               />
               <Project
-                description='TypeScript SDK for the Spotify Web API'
+                bgColor='white'
+                description={
+                  <span>
+                    {'Top contributor to '}
+                    <span className='font-mono text-sm bg-neutral-300 dark:bg-neutral-700 px-1 py-0.5 rounded-md'>@spotify/web-api-ts-sdk</span>
+                    {' - Spotify\'s official TypeScript SDK for the Spotify Web API. '}
+                    <a className='hover:text-rose-500 transition font-medium' href='#rabbit'>Rabbit</a>
+                    {' uses this npm package.'}
+                  </span>
+                }
                 href='https://github.com/spotify/spotify-web-api-ts-sdk'
                 id='spotify'
+                srcIcon='https://raw.githubusercontent.com/npm/logos/master/npm%20square/n-64.png'
                 title='Spotify Web API SDK'
               />
             </div>
