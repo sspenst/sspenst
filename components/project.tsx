efe093269d46b4fa0e6acf0fcf8d43c23fdefa39
Spencer Spenst <spencerspenst@gmail.com>
2024-04-01 22:55:31 -0700

    github link tweak

--- components/project.tsx
+++ components/project.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import React from 'react';
-import Github from './icons/github';
 
 interface ProjectProps {
   bgColor: string;
@@ -40,18 +39,15 @@ export default function Project({ bgColor, children, githubHref, href, id, srcIc
       {githubHref &&
         <a
           aria-label={`${id} source code`}
-          className='text-neutral-600 dark:text-neutral-400 hover:text-black dark:hover:text-white transition flex items-center gap-2.5 text-sm'
+          className='text-neutral-600 dark:text-neutral-400 hover:text-black dark:hover:text-white transition flex items-center gap-2 text-sm'
           href={githubHref}
           rel='noreferrer'
           target='_blank'
         >
-          <div className='w-6 h-6' style={{
-            minHeight: 24,
-            minWidth: 24,
-          }}>
-            <Github />
-          </div>
-          Code
+          <span>View on GitHub</span>
+          <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-4 h-4'>
+            <path strokeLinecap='round' strokeLinejoin='round' d='M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25' />
+          </svg>
         </a>
       }
     </div>
