edb06b3813d83cff7cf2b1e595f24e0289d67754
Spencer Spenst <spencerspenst@gmail.com>
2026-09-24 10:32:41 -0700

    home page text tweaks

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,10 +23,10 @@ export default function Index() {
             Before this, I worked at <FaviconLink href='https://microsoft.com'>Microsoft</FaviconLink> to help build out the Universal Store, add new pricing systems, and support new product types.
           </p>
           <p>
-            I graduated from <FaviconLink href='https://www.ubc.ca/'>UBC</FaviconLink> in Computer Engineering. While I was there I had the chance to work as a research assistant focusing on GPU optimizations for machine learning.
+            I graduated from <FaviconLink href='https://ubc.ca'>UBC</FaviconLink> in Computer Engineering. While I was there I had the chance to work as a research assistant focusing on GPU optimizations for machine learning.
           </p>
           <p>
-            I like to build solutions to problems I see in my own life. I made <FaviconLink href='https://chromewebstore.google.com/detail/simplify-youtube/lcakcdjbimeedkincdcpphddbjdmdhaa'>Simplify YouTube</FaviconLink> to remove Shorts and other distracting UX elements. I was also a top contributor to the <FaviconLink href='https://www.npmjs.com/package/@sspenst/spotify-web-api'>Spotify Web API</FaviconLink> so that I could build <FaviconLink href='https://rabbit.sspenst.com'>Rabbit</FaviconLink>, a quick way to discover new music.
+            I like to build solutions to problems I see in my own life. I made <FaviconLink href='https://chromewebstore.google.com/detail/simplify-youtube/lcakcdjbimeedkincdcpphddbjdmdhaa'>Simplify YouTube</FaviconLink> to remove Shorts and other distracting UX elements, and I was a top contributor to the <FaviconLink href='https://www.npmjs.com/package/@sspenst/spotify-web-api'>Spotify Web API</FaviconLink> so I could build <FaviconLink href='https://rabbit.sspenst.com'>Rabbit</FaviconLink>, a quick way to discover new music.
           </p>
           <p>
             I&apos;ve also made some music myself which you can check out <TransitionLink className='font-medium hover:text-rose-500 transition' href='/music'>here</TransitionLink>.
@@ -35,7 +35,7 @@ export default function Index() {
             Feel free to connect through <FaviconLink href='https://github.com/sspenst'>GitHub</FaviconLink>, <FaviconLink href='https://linkedin.com/in/sspenst'>LinkedIn</FaviconLink>, or <a className='whitespace-nowrap font-medium hover:text-rose-500 transition' href='mailto:spencerspenst@gmail.com'><EnvelopeIcon aria-hidden='true' className='inline-block w-4 h-4 mr-1 align-[-0.125em]' />email</a>!
           </p>
           <p className='text-end text-neutral-500'>
-            September 22, 2026
+            September 24, 2026
           </p>
         </RevealText>
       </div>
